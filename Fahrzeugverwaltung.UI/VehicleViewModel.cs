6ced58af15493aae03372e7d495374d81ae77263
Tanyel Tuncer <tanyel.tuncer@live.com>
2021-12-22 20:51:16 +0100

    Added ui

---
+++ Fahrzeugverwaltung.UI/VehicleViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+
+namespace Fahrzeugverwaltung.UI
+{
+    public class VehicleViewModel
+    {
+        private Vehicle vehicle;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public VehicleViewModel()
+        {
+            vehicle = new Vehicle();
+        }
+
+        public string Type { get => vehicle.Type; set => vehicle.Type = value; }
+        public string Model { get => vehicle.Model; set => vehicle.Model = value; }
+
+    }
+}
