572251790443db7feeeb16b45a8534ab803bd21f
Tanyel Tuncer <tanyel.tuncer@live.com>
2022-01-25 22:24:18 +0100

    added comments and change anlegen to speichern

--- Fahrzeugverwaltung.UI/VehicleViewModel.cs
+++ Fahrzeugverwaltung.UI/VehicleViewModel.cs
@@ -1,21 +1,43 @@
-﻿using System.Windows;
+﻿using GalaSoft.MvvmLight.Command;
+using System;
+using System.Windows;
+using System.Windows.Input;
 
 namespace Fahrzeugverwaltung.UI
 {
     public class VehicleViewModel
     {
         private Vehicle vehicle;
+        private ICommand saveCommand;
 
         /// <summary>
         /// Constructor
         /// </summary>
         public VehicleViewModel()
         {
+            saveCommand = new RelayCommand(Save);
             vehicle = new Vehicle();
         }
 
+        private void Save()
+        {
+            MessageBox.Show("");
+        }
+
+        /// <summary>
+        /// Gets or sets a type of a <see cref="Vehicle"/>.
+        /// </summary>
         public string Type { get => vehicle.Type; set => vehicle.Type = value; }
+
+        /// <summary>
+        /// Gets or sets a model of a <see cref="Vehicle"/>.
+        /// </summary>
         public string Model { get => vehicle.Model; set => vehicle.Model = value; }
+
+        /// <summary>
+        /// Gets or sets a save command
+        /// </summary>
+        public ICommand SaveCommand { get => saveCommand; set => saveCommand = value; }
 
     }
 }
