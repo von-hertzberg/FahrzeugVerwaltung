bd7fa1513db5a471d58a581cc7a659157bfb3528
von-hertzberg <paul.dikty@simplic.biz>
2022-11-10 16:53:21 +0100

    started rework of task 2

---
+++ Fahrzeugverwaltung.UI/VehicleDialog.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace FahrzeugVerwaltung.UI
+{
+    /// <summary>
+    /// Interaction logic for VehicleDialog.xaml
+    /// </summary>
+    public partial class VehicleDialog : Window
+    {
+        public Vehicle Vehicle { get; set; }
+
+        public VehicleDialog(Vehicle vehicle)
+        {
+            InitializeComponent();
+
+            this.Vehicle = vehicle;
+            DataContext = Vehicle;
+        }
+
+        public VehicleDialog() : this(new Vehicle())
+        { }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (Vehicle.Type is null ||
+                Vehicle.Brand is null ||
+                Vehicle.Model is null ||
+                Vehicle.Type == string.Empty ||
+                Vehicle.Brand == string.Empty ||
+                Vehicle.Model == string.Empty)
+            {
+                MessageBox.Show("Input not complete");
+            }
+            else
+            {
+                DialogResult = true;
+            }
+        }
+
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+        }
+    }
+}
--- Fahrzeugverwaltung.UI/VehicleViewModel.cs
+++ Fahrzeugverwaltung.UI/VehicleViewModel.cs
@@ -1,120 +1,58 @@
 ﻿using GalaSoft.MvvmLight.Command;
-using System;
-using System.Collections.Generic;
+using Microsoft.Win32;
 using System.Collections.ObjectModel;
-using System.Windows;
+using System.IO;
+using System.Text.Json;
 using System.Windows.Input;
 
 namespace FahrzeugVerwaltung.UI
 {
     public class VehicleViewModel
     {
-        private Vehicle vehicle;
-        private ICommand saveCommand;
-        private ICommand newCommand;
-        private ICommand editCommand;
-        private ICommand deleteCommand;
-        private ObservableCollection<Vehicle> vehicles;
-        int selectedVehicleIndex;
+        public ObservableCollection<Vehicle> Vehicles { get; set; }
 
-        /// <summary>
-        /// Constructor
-        /// </summary>
+        public ICommand NewVehicleCommand { get; set; }
+        public ICommand EditVehicleCommand { get; set; }
+        public ICommand DeleteVehicleCommand { get; set; }
+
         public VehicleViewModel()
         {
-            saveCommand = new RelayCommand(Save);
-            newCommand = new RelayCommand(New);
-            editCommand = new RelayCommand(Edit);
-            deleteCommand = new RelayCommand(Delete);
-            vehicle = new Vehicle();
-            vehicles = new ObservableCollection<Vehicle>();
-
-            FillVehicleList();
-            selectedVehicleIndex = -1;
-        }
-
-        private void FillVehicleList()
-        {
-            Random random = new Random();
-
-            string[] types = { "PKW", "LKW" };
-
-            // type -> brand
-            Dictionary<string, string[]> brands = new Dictionary<string, string[]>();
-            brands["PKW"] = new string[] { "VW" };
-            brands["LKW"] = new string[] { "Mercedes" };
-
-            // brand -> model
-            Dictionary<string, string[]> models = new Dictionary<string, string[]>();
-            models["VW"] = new string[] { "Käfer", "Polo", "Touran" };
-            models["Mercedes"] = new string[] { "LKW1" };
-
-            var vehicleCount = random.Next(3, 6);
-            for (int i = 0; i < vehicleCount; i++)
-            {
-                vehicle.Type = types[random.Next(types.Length)];
-
-                var brandList = brands[vehicle.Type];
-                vehicle.Brand = brandList[random.Next(brandList.Length)];
-
-
-                var modelList = models[vehicle.Brand];
-                vehicle.Model = modelList[random.Next(modelList.Length)];
-
-                vehicles.Add(vehicle);
-                vehicle = new Vehicle();
-            }
+            NewVehicleCommand = new RelayCommand(New);
+            EditVehicleCommand = new RelayCommand(Edit);
+            DeleteVehicleCommand = new RelayCommand(Delete);
+            Vehicles = new ObservableCollection<Vehicle>();
         }
 
         private void Save()
         {
-            MessageBox.Show(string.Format("Es wurde folgendes Fahrzeug angelegt:\n{0}", vehicle));
+            var dialog = new SaveFileDialog();
 
-            vehicles.Add(vehicle);
-            vehicle = new Vehicle();
+            if (!dialog.ShowDialog().GetValueOrDefault(false))
+                return;
+
+            var json = JsonSerializer.Serialize(Vehicles);
+            using (var file = File.CreateText(dialog.FileName))
+            {
+                file.Write(json);
+            }
         }
 
         private void New()
         {
+            var dialog = new VehicleDialog();
+            dialog.ShowDialog();
 
+            if (dialog.DialogResult.GetValueOrDefault(false))
+            {
+                Vehicles.Add(dialog.Vehicle);
+            }
         }
 
         private void Edit()
         {
-
         }
         private void Delete()
         {
-            if (selectedVehicleIndex > -1 && selectedVehicleIndex < vehicles.Count)
-            {
-                vehicles.RemoveAt(selectedVehicleIndex);
-            }
-            selectedVehicleIndex = -1;
         }
-
-        /// <summary>
-        /// Gets or sets a type of a <see cref="Vehicle"/>.
-        /// </summary>
-        public string Type { get => vehicle.Type; set => vehicle.Type = value; }
-
-        /// <summary>
-        /// Gets or sets a model of a <see cref="Vehicle"/>.
-        /// </summary>
-        public string Model { get => vehicle.Model; set => vehicle.Model = value; }
-
-        public string Brand { get => vehicle.Brand; set => vehicle.Brand = value; }
-
-        /// <summary>
-        /// Gets or sets a save command
-        /// </summary>
-        public ICommand SaveCommand { get => saveCommand; set => saveCommand = value; }
-        public ICommand NewCommand { get => newCommand; set => newCommand = value; }
-        public ICommand EditCommand { get => editCommand; set => editCommand = value; }
-        public ICommand DeleteCommand { get => deleteCommand; set => deleteCommand = value; }
-
-        public ObservableCollection<Vehicle> VehicleList { get => vehicles; }
-
-        public int SelectedVehicleIndex { get => selectedVehicleIndex; set => selectedVehicleIndex = value; }
-
     }
 }
